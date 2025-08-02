7b17a25d08eb4d9d3901dce2c1f2e8c2a22a0ddf
gubai <470014599@qq.com>
2025-08-02 11:18:55 +0800

    style: 优化格式

---
+++ src/main.py
@@ -0,0 +1,97 @@
+from cookie import BilibiliQRLogin
+from video_download import BiliVideoDownloader
+from comments import BiliCommentsFetcher
+
+def main_menu():
+    print("""
+          ====== Bilibili Video Downloader ======
+          1. 账号管理
+          2. 下载
+          q. 退出
+          =======================================""")
+    choice: str = input("请输入选项：").strip().lower()
+    if choice == "1":
+        user_menu()
+    elif choice == "2":
+        video_menu()
+    elif choice == "q":
+        exit()
+    else:
+        print("输入错误，请重新输入！")
+        main_menu()
+
+def user_menu():
+    login = BilibiliQRLogin()
+    print("""
+          ====== Bilibili Video Downloader ======
+          1. 扫码登陆并获取cookie
+          2. 显示当前登陆状态
+          3. 显示当前cookie
+          4. 转换为Playwright格式Cookie
+          5. 注销登录
+          6. 上一步
+          q. 退出
+          =======================================""")
+    choice = input("请输入选项：").strip().lower()
+    if choice == "q":
+        exit()
+    elif choice == "6":
+        main_menu()
+    elif choice == "1":
+        login.qr_login()
+        user_menu()
+    elif choice == "2":
+        login.is_logged_in()
+        user_menu()
+    elif choice == "3":
+        login.show_cookies()
+        user_menu()
+    elif choice == "4":
+        login.convert_cookies_for_playwright()
+        user_menu()
+    elif choice == "5":
+        if login.is_logged_in():
+            confirm = input("确定要注销吗? (y/n): ").strip().lower()
+            if confirm == "y":
+                login.logout()
+        else:
+            print("当前未登录")
+        user_menu()
+    else:
+        print("输入错误，请重新输入")
+     
+def video_menu():
+    print("""
+          ====== Bilibili Video Downloader ======
+          1. 下载视频
+          2. 下载评论
+          3. 上一步
+          q. 退出
+          =======================================""")
+    choice = input("请输入选项：").strip().lower()
+    if choice == "q":
+        exit()
+    elif choice == "1":
+        download_video(BiliVideoDownloader())
+    elif choice == "2":
+        download_comments(BiliCommentsFetcher())
+    elif choice == "3":
+        main_menu()
+    else:
+        print("输入错误，请重新输入！")
+        video_menu()
+    
+    
+def download_video(downloader: BiliVideoDownloader):
+    downloader.run()
+    video_menu()
+    
+def download_comments(fetcher: BiliCommentsFetcher):
+    fetcher.run()
+    video_menu()
+    
+def main():
+    main_menu()
+
+if __name__ == "__main__":
+    main()
